5a63e799e551a704e6826f87c59c4adc5c63c4c7
fergalrooney <fergalr@gmail.com>
2014-03-20 21:08:15 -0400

    Empty Project Application Commit
    Initial empty template project commit

---
+++ WeatherFergCast/WeatherFergCast/WFCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WFCAppDelegate.h
+//  WeatherFergCast
+//
+//  Created by Fergal Rooney on 3/20/14.
+//  Copyright (c) 2014 Fergal Rooney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WFCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WeatherFergCast/WeatherFergCast/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WeatherFergCast
+//
+//  Created by Fergal Rooney on 3/20/14.
+//  Copyright (c) 2014 Fergal Rooney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WFCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WFCAppDelegate class]));
+    }
+}
